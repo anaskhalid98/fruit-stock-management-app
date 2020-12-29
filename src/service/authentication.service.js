34ceab4a2889cb574a360448f5234b6ee618353b
AnasKhalid <anass.khalid5@gmail.com>
2020-12-29 15:16:36 +0100

    Add project Docs using Jsdoc

--- src/service/authentication.service.js
+++ src/service/authentication.service.js
@@ -2,6 +2,12 @@ import {request} from "../config/axios-config";
 import {API_BASE_URL} from "../constants";
 
 
+/**
+ * Login
+ *
+ * @param {Object} Object made of username and password.
+ * @returns {Object} JWT token
+ */
 export function login(loginRequest) {
 	return request({
 		url: API_BASE_URL + "/api/auth/signin",
@@ -9,6 +15,12 @@ export function login(loginRequest) {
 		data: JSON.stringify(loginRequest),
 	});
 }
+
+/**
+ * signUp
+ * @param {Object} Object made of username, email and password.
+ * @returns {Object} Success message
+ */
 export function signUp(signUpRequest) {
 	return request({
 		url: API_BASE_URL + "/api/auth/signup",
@@ -17,6 +29,10 @@ export function signUp(signUpRequest) {
 	});
 }
 
+/**
+ * signUp
+ * @returns {Object} UserStock
+ */
 export function getUserStock() {
 	return request({
 		url: API_BASE_URL + "/api/Stock",
